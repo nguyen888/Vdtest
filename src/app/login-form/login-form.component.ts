e3c045046d682297dbb56c727e721ba6bc4c309b
nguyenduycongndc <nguyenduycongndc@gmail.com>
2020-04-23 16:13:17 +0700

    23.4.2020

---
+++ src/app/login-form/login-form.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login-form',
+  templateUrl: './login-form.component.html',
+  styleUrls: ['./login-form.component.css']
+})
+export class LoginFormComponent implements OnInit {
+  avatar: any= {};
+  constructor() { }
+
+  ngOnInit(): void {
+    this.avatar.url = 'https://i.9mobi.vn/cf/images/ba/2018/4/16/anh-avatar-dep-4.jpg';
+
+  }
+
+}
